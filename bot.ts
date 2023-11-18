c77b03a557432b7bb347cdde465ff7d308c8c52a
ManuRC2 <manuelcolusso@live.com.ar>
2023-11-18 15:22:00 -0300

    Fix multiples mensajes

--- bot.ts
+++ bot.ts
@@ -16,6 +16,35 @@ var datestring: string;
 var chat_ids: number[]
 var mensaje: string
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+    switch(messageText) { 
+        case '/start': { 
+            bot.sendMessage(chatId, 'Hola! Mi funcion es notificarte si hay stock de notebooks de Argentina Programa.\n Para ayuda, manda /help');
+            break; 
+        } 
+        case '/help': { 
+            bot.sendMessage(chatId, 'Comandos:\n/start: iniciar el bot\n/help: mostrar este mensaje\n/guardar: agregarte a la lista de numeros\n/sacar: sacarte de la lista de numeros');
+            break; 
+        } 
+        case '/guardar': { 
+            console.log(agregar_numero(chatId))
+            bot.sendMessage(chatId, 'Listo ahi te agregue master');
+            break; 
+        } 
+        case '/sacar': {
+            console.log(sacar_numero(chatId))
+            bot.sendMessage(chatId, 'Listo loco ya te saque');
+            break; 
+        } 
+        default: { 
+            bot.sendMessage(chatId, 'No entendi nada, manda /help para ayuda');
+            break; 
+        } 
+    }
+});
+
 export async function fetchStock(url: string){
     return axios.get(url, {timeout: 1000}).then((response) => {
         $ = cheerio.load(response.data);
@@ -47,33 +76,5 @@ export async function fetchStock(url: string){
 console.log("Starting...")
 while(true) {
     await fetchStock("https://especiales.tiendabna.com.ar/catalog/promo-argentina-programa");
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-        const messageText = msg.text;
-        switch(messageText) { 
-            case '/start': { 
-                bot.sendMessage(chatId, 'Hola! Mi funcion es notificarte si hay stock de notebooks de Argentina Programa.\n Para ayuda, manda /help');
-                break; 
-            } 
-            case '/help': { 
-                bot.sendMessage(chatId, 'Comandos:\n/start: iniciar el bot\n/help: mostrar este mensaje\n/guardar: agregarte a la lista de numeros\n/sacar: sacarte de la lista de numeros');
-                break; 
-            } 
-            case '/guardar': { 
-                console.log(agregar_numero(chatId))
-                bot.sendMessage(chatId, 'Listo ahi te agregue master');
-                break; 
-            } 
-            case '/sacar': {
-                console.log(sacar_numero(chatId))
-                bot.sendMessage(chatId, 'Listo loco ya te saque');
-                break; 
-            } 
-            default: { 
-                bot.sendMessage(chatId, 'No entendi nada, manda /help para ayuda');
-                break; 
-            } 
-        }
-    });
     await sleep(COOLDOWN);
 }
